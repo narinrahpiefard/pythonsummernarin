add75b66fff6ffb8a207e4257549ec09c9c57320
narin <narinrahpiefard@gmail.com>
2024-07-29 21:28:44 +0330

    Create q6.py

---
+++ tamrin3/q6.py
@@ -0,0 +1,11 @@
+a=eval(input("enter a number: "))
+b=eval(input("enter a number: "))
+c=eval(input("enter a number: "))
+d=eval(input("enter a number: "))
+e=eval(input("enter a number: "))
+f=a+b+c+d+e
+if f/2==0:
+    g=eval(input("enter new number: "))
+    print(a+b+c+d+e+g)
+elif f/2!=0:
+    print(a,b,d,e)
