58b1a1cfe09af84f69b00b86accb0d3fbda73a16
narin <narinrahpiefard@gmail.com>
2024-07-30 11:33:23 +0330

    Update q1.py

--- tamrin3/q1.py
+++ tamrin3/q1.py
@@ -2,6 +2,7 @@ speed=eval(input("enter your speed: "))
 if speed < 90:
     print("no ticket!")
 elif speed <= 91 and speed < 110:
+elif speed >= 91 and speed < 110:
     print("small ticket!")
 else:
     print("big ticket!")
