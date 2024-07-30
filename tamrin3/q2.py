85a9034ee008785b214d743739f3842dbbbb37c3
narin <narinrahpiefard@gmail.com>
2024-07-30 11:36:29 +0330

    Update q2.py

--- tamrin3/q2.py
+++ tamrin3/q2.py
@@ -1,7 +1,7 @@
 year=eval(input("enter year: "))
-if year/4==0:
+if year%4==0:
     print("kabise")
-elif year/4==0 and year/100==0:
+elif year%4==0 and year%100==0:
     print("kabise nist")
 else:
     print("kabise nist")
